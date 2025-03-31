ed336a08b3ce3490ec76578d8e69ed842b84ca50
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-31 12:26:53 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 sudo apt install musl-tools -y
-git clone --depth=1 https://git.suckless.org/sbase/
-cp config.mk  sbase/
-cd sbase
+repo=sinit
+echo $repo > repo
+git clone --depth=1 https://git.suckless.org/$repo/
+cp config.mk  $repo/
+cd $repo
 make dist
 make DESTDIR="$PWD/pkg" install
-tar -czvf sbase-static.tar.gz -C "$PWD/pkg" .
+tar -czvf $repo-static.tar.gz -C "$PWD/pkg" .
 
-# make all install uninstall dist sbase-box-install sbase-box-uninstall clean
-# links to /lib/x86_64-linux-gnu/libc.so if build from debian
 
